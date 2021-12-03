b99435005b1514856cf807fe525cddad4b4dcb15
olga <arriba-en@ya.ru>
2021-12-03 09:08:48 +0300

    7 урок

---
+++ HW7/ex1.py
@@ -0,0 +1,49 @@
+# Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса (метод __init__()),
+# который должен принимать данные (список списков) для формирования матрицы.
+# Подсказка: матрица — система некоторых математических величин, расположенных в виде прямоугольной схемы.
+# Следующий шаг — реализовать перегрузку метода __str__() для вывода матрицы в привычном виде.
+# Далее реализовать перегрузку метода __add__() для реализации операции сложения двух объектов
+# класса Matrix (двух матриц). Результатом сложения должна быть новая матрица.
+# Подсказка: сложение элементов матриц выполнять поэлементно —
+# первый элемент первой строки первой матрицы складываем с первым элементом первой строки второй матрицы и т.д.
+
+class Matrix():
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+
+    def __str__(self):
+        n_m = list()
+        n_m1 = str()
+
+        for line in self.list_of_list:
+            for i, v in enumerate(line):
+                n_m.append(v)
+                if i == len(line) - 1:
+                    n_m.append('\n')
+                n_m1 = '\t'.join(map(str, n_m))
+
+        return f'\t{n_m1}'
+
+    def __add__(self, other):
+        nw_mtrx = [[0 for x in range(len(self.list_of_list[0]))] for y in range(len(self.list_of_list))]
+
+        if len(self.list_of_list) == len(other.list_of_list):
+            if len(self.list_of_list[0]) == len(other.list_of_list[0]):
+                for i in range(len(self.list_of_list)):
+                    for j in range(len(self.list_of_list[i])):
+                        nw_mtrx[i][j] = self.list_of_list[i][j] + other.list_of_list[i][j]
+
+                return Matrix(nw_mtrx)
+            else:
+                return f'Invalid matrix size'
+
+
+my_matrix1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9], [3, 2, 1]])
+
+my_matrix2 = Matrix([[9, 8, 7], [6, 5, 4], [3, 2, 1], [3, 2, 1]])
+
+n_mt = my_matrix1 + my_matrix2
+print(n_mt)
+
+print(my_matrix1)
+print(my_matrix2)
---
+++ HW7/ex2.py
@@ -0,0 +1,48 @@
+# Реализовать проект расчета суммарного расхода ткани на производство одежды.
+# Основная сущность (класс) этого проекта — одежда, которая может иметь определенное название.
+# К типам одежды в этом проекте относятся пальто и костюм.
+# У этих типов одежды существуют параметры: размер (для пальто) и рост (для костюма).
+# Это могут быть обычные числа: V и H, соответственно.
+# Для определения расхода ткани по каждому типу одежды использовать формулы:
+# для пальто (V/6.5 + 0.5), для костюма (2 * H + 0.3). Проверить работу этих методов на реальных данных.
+# Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания:
+# реализовать абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property.
+
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def amount_of_fabric(self):
+        pass
+
+
+class Sute(Clothes):
+    def __init__(self, name, size):
+        super().__init__(name)
+        self.size = size
+
+    def amount_of_fabric(self):
+        return (self.size / 6.5) + 0.5
+
+
+class Coat(Clothes):
+    def __init__(self, name, higt):
+        super().__init__(name)
+        self.higt = higt
+
+    def amount_of_fabric(self):
+        return (self.higt * 2) + 0.3
+
+
+new_coat = Coat('from market', 2)
+new_sute = Sute('from shop', 4)
+
+print(new_coat.amount_of_fabric())
+print(new_sute.amount_of_fabric())
+
+total_f=new_coat.amount_of_fabric()+new_sute.amount_of_fabric()
+print(total_f)
