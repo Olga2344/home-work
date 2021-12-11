6b2fd248f38d6326adadf93717aa02a81b251e89
olga <arriba-en@ya.ru>
2021-12-11 18:17:20 +0300

    8 урок

--- HW8/ex4.py
+++ HW8/ex4.py
@@ -11,28 +11,33 @@
 # Например, для указания количества принтеров, отправленных на склад, нельзя использовать строковый тип данных.
 
 class Warehouse:
-    def __init__(self, amount_of_shelves, amount_of_levels):
-        self.amount_of_shelves = amount_of_shelves
-        self.amount_of_levels = amount_of_levels
-        # storege= '''сделать матрицу хранения'''
+    def __init__(self, items_list):
+        # self.amount_of_shelves = amount_of_shelves
+        # self.amount_of_levels = amount_of_levels
+        self.items_list = items_list
 
-    def accept_item(self):
-        pass
+    def accept_item(self, item):
+        self.items_list.append(item)
 
-    def issue_item(self):
-        pass
+    def issue_item(self, item):
+        if item in self.items_list:
+            self.items_list.remove(item)
 
 
 class Equipment:
     def __init__(self, brand, serial_number):
         self.brand = brand
-        self.serial_number=serial_number
+        self.serial_number = serial_number
 
     def action(self):
         pass
 
+    def __repr__(self):
+        return f'{self.brand}, {self.serial_number}'
+
+
 class Printer(Equipment):
-    def __init__(self, brand, func,serial_number):
+    def __init__(self, brand, func, serial_number):
         super().__init__(brand, serial_number)
         self.func = func
 
@@ -50,9 +55,22 @@ class CopyCenter(Equipment):
 
 
 class Laptop(Equipment):
-    def __init__(self, brand, serial_number, func, ):
+    def __init__(self, brand, func, serial_number):
         self.func = func
         super().__init__(brand, serial_number)
 
     def action(self):
         print('работает')
+
+
+nw_printer = Printer('HP', 'print', 1234)
+nw_laptop = Laptop('HP', 'codding', 1233)
+nw_copy = CopyCenter('HP', 'copy', 1232)
+nw_copy1 = CopyCenter('HP', 'copy', 1235)
+
+my_warehouse = Warehouse([nw_copy, nw_laptop, nw_printer])
+print(my_warehouse.items_list)
+my_warehouse.accept_item(nw_copy1)
+print(my_warehouse.items_list)
+my_warehouse.issue_item(nw_laptop)
+print(my_warehouse.items_list)
